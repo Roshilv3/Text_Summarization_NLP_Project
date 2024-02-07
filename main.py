0d8bdf402dbfd2d318d443b76bc5f35f8692034e
Roshilv3 <roshil.verma.3@gmail.com>
2024-02-07 14:57:20 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from src.text_summarization.pipeline.stage01_data_ingestion import *
+from src.text_summarization.pipeline.stage02_data_validation import *
 from src.text_summarization.logger import logging
 
-STAGE_NAME = "Data Ingestion Stage"
-
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+STAGE_NAME_01 = "DATA INGESTION STAGE"
 try:
-    logging.info(f">>>stage {STAGE_NAME} started<<<")
+    logging.info(f">>>stage {STAGE_NAME_01} started<<<")
     data_ingestion = DataIngestionPipeline()
     data_ingestion.main()
-    logging.info(f">>>stage {STAGE_NAME} completed<<<")
+    logging.info(f">>>stage {STAGE_NAME_01} completed<<<")
 except Exception as e:
     raise e
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+STAGE_NAME_02 = "DATA VALIDATION STAGE"
+try:
+    logging.info(f">>>stage {STAGE_NAME_02} started<<<")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
+    logging.info(f">>>stage {STAGE_NAME_02} completed<<<")
+except Exception as e:
+    raise e
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
